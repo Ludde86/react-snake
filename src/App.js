3452eeb157bc7438e7c3bebef09c85139150e381
Ludde86 <bslbjorn@gmail.com>
2019-09-30 22:14:25 +0200

    edit snake position

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import React from "react";
+import React, { Component } from "react";
+import Snake from "./Snake";
 import "./index.css";
 
-function App() {
-  return <div className='game-area'>
-    <div className="snake-dot" style={{top:0, left:0}}></div>
-  </div>;
+class App extends Component {
+  state = {
+    snakeDots: [[0, 0], [2, 0]]
+  };
+  render() {
+    return (
+      <div className='game-area'>
+        <Snake snakeDots={this.state.snakeDots} />
+      </div>
+    );
+  }
 }
 
 export default App;
