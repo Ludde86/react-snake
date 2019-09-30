b3c70e9001fb7dd863154719453d031133416349
Ludde86 <bslbjorn@gmail.com>
2019-09-30 21:34:00 +0200

    snake start position

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import React from 'react';
+import React from "react";
+import "./index.css";
 
 function App() {
-  return (
-    <div>
-      
-    </div>
-  );
+  return <div className='game-area'>
+    <div className="snake-dot" style={{top:0, left:0}}></div>
+  </div>;
 }
 
 export default App;
