e2910d08d79ed41488bb5bf6322262f1f50d0a6d
Ludde86 <bslbjorn@gmail.com>
2019-10-01 19:46:48 +0200

    implemented food

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import React, { Component } from "react";
 import Snake from "./Snake";
 import "./index.css";
+import Food from "./Food";
 
 class App extends Component {
   state = {
+    food: [6, 8],
     snakeDots: [[0, 0], [2, 0]]
   };
   render() {
     return (
       <div className='game-area'>
         <Snake snakeDots={this.state.snakeDots} />
+        <Food dot={this.state.food} />
       </div>
     );
   }
